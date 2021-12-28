b95464379de681475577833fba3f70354f2991d7
Andrew (LyfeOnEdge) (ArcticGentoo) <arcticgentoo@gmail.com>
2021-12-27 19:40:08 -0800

    Update opencatan.py

--- opencatan.py
+++ opencatan.py
@@ -17,19 +17,19 @@ def get_random_color():
 	return ursina.color.rgb(random.randint(0,255),random.randint(0,255),random.randint(0,255))
 
 sqrt3 = math.sqrt(3)
-def _calc_hexagon_verts(x_offset, z_offset, x,z,r,y=0, terrain_amplification = 1,terrain_scale=1):
-	x*=r #Scale
-	z*=r
+def _calc_hexagon_verts(x,z,r,y=0, terrain_amplification = 1,terrain_scale=1):
 	hr = r*0.5
 	sqrt3hr = sqrt3*hr
-	verts = [(v[0]+x_offset,y+get_heightmap(v[0]/terrain_scale,v[1]/terrain_scale)*terrain_amplification*0.5,v[1]+z_offset) for v in[
-		(x, r + z),
-		(sqrt3hr + x, hr + z),
-		(sqrt3hr + x, -hr + z),
-		(x, -r + z),
-		(-sqrt3hr + x, -hr + z),
-		(-sqrt3hr + x, hr + z)
-	]]
+	verts = [(float("{:.5f}".format(v[0])), float("{:.5f}".format(y+get_heightmap((v[0]+x*r)/terrain_scale,(v[1]+z*r)/terrain_scale)*terrain_amplification*0.5)), float("{:.5f}".format(v[1]))) for v in[
+				(0, r),
+				(sqrt3hr, hr),
+				(sqrt3hr, -hr),
+				(0, -r),
+				(-sqrt3hr, -hr),
+				(-sqrt3hr, hr)
+			]
+		
+	]
 	return verts
 
 def calc_hexagon_verts_from_center_point(*args, **kwargs):
@@ -63,27 +63,91 @@ def draw_honeycomb(position,radius,scale,terrain_amplification,terrain_scale):
 	origin_x, origin_z = position
 	points = calc_hex_grid_points_from_radius(radius)
 	tiles=[]
-	unique_verticies = set()
 	for row in points:
 		for x,z in row:
+			position=((origin_x+x*scale),0,(origin_z+z*scale))
 			color = get_resource_color(pick_random_resource_type())
-			p = calc_hexagon_verts_from_center_point(origin_x, origin_z, x, z, scale, 0, terrain_amplification=terrain_amplification,terrain_scale=terrain_scale)
-			linepoints = calc_hexagon_outline_verts_from_center_point(origin_x, origin_z, x, z, scale, 0, terrain_amplification=terrain_amplification,terrain_scale=terrain_scale)
-			t = Tile(p, ursina.color.colors[color])
-			t.outline = ursina.Entity(model=ursina.Mesh(vertices=(linepoints[0]), triangles=linepoints[1], mode='line', thickness=int(3*min(1,scale))), color=ursina.color.rgb(0,0,0), y = 0.02)
+			p = calc_hexagon_verts_from_center_point(x, z, scale, 0, terrain_amplification=terrain_amplification,terrain_scale=terrain_scale)
+			linepoints = calc_hexagon_outline_verts_from_center_point(x, z, scale, 0, terrain_amplification=terrain_amplification,terrain_scale=terrain_scale)
+			t = Tile(position, p, ursina.color.colors[color])
+			t.outline = ursina.Entity(model=ursina.Mesh(vertices=(linepoints[0]), triangles=linepoints[1], mode='line', thickness=int(3*min(1,scale))), position=position, color=ursina.color.rgb(0,0,0), y = 0.02)
 			tiles.append(t)
-			
 	return tiles
 
-class Tile(ursina.Entity):
-	def __init__(self, points, color):
+#Horribly slow but constructs the hexagon grid
+# def draw_honeycomb_new(position,radius,scale,terrain_amplification,terrain_scale):
+# 	origin_x, origin_z = position
+# 	points = calc_hex_grid_points_from_radius(radius)
+
+# 	tiles=[]
+# 	unique_edges = set()
+# 	for row in points:
+# 		for x,z in row:
+# 			color = get_resource_color(pick_random_resource_type())
+# 			p = calc_hexagon_verts_from_center_point(origin_x, origin_z, x, z, scale, 0, terrain_amplification=terrain_amplification,terrain_scale=terrain_scale)
+# 			linepoints = calc_hexagon_outline_verts_from_center_point(origin_x, origin_z, x, z, scale, 0, terrain_amplification=terrain_amplification,terrain_scale=terrain_scale)
+# 			t = Tile(p, ursina.color.colors[color])
+
+# 			line_verts, line_tris = linepoints
+# 			for i in range(0, len(line_verts)-1):
+# 				if line_verts[i][0] > line_verts[i+1][0]:
+# 					unique_edges.add((line_verts[i], line_verts[i+1]))
+# 				else:
+# 					unique_edges.add((line_verts[i+1], line_verts[i]))
+# 			if line_verts[len(line_verts)-1][0] > line_verts[0][0]:
+# 				unique_edges.add((line_verts[len(line_verts)-1], line_verts[0]))
+# 			else:
+# 				unique_edges.add((line_verts[0], line_verts[len(line_verts)-1]))
+# 			# t.outline = ursina.Entity(model=ursina.Mesh(vertices=(linepoints[0]), triangles=linepoints[1], mode='line', thickness=int(3*min(1,scale))), color=ursina.color.rgb(0,0,0), y = 0.02)
+# 			tiles.append(t)
+# 	for e in unique_edges:
+# 		print(e)
+
+# 	verts = []
+# 	tris = []
+# 	for e in unique_edges:
+# 		first_index = 0
+# 		second_index = 0
+# 		e0, e1 =e
+# 		if e0 in verts:
+# 			first_index = verts.index(e0)
+# 		else:
+# 			verts.append(e0)
+# 			first_index = len(verts)-1
+# 		if e1 in verts:
+# 			second_index = verts.index(e1)
+# 		else:
+# 			verts.append(e1)
+# 			second_index = len(verts)-1
+# 		tris.append((first_index, second_index))
+# 		gridmesh = ursina.Entity(model=ursina.Mesh(vertices=verts, triangles=tris, mode='line', thickness=3), color=ursina.color.rgb(0,0,0), y = 0.02)
+
+# 	print(f"Counted {len(unique_edges)} verts")
+# 	return tiles
+
+class Tile(ursina.Button):
+	def __init__(self, position, points, color):
 		self.verts, self.tris = points
-		ursina.Entity.__init__(self, model=ursina.Mesh(vertices=self.verts, triangles=self.tris, mode='ngon', thickness=1), color=color)
+		ursina.Button.__init__(
+			self,
+			parent=ursina.scene,
+			model=ursina.Mesh(vertices=self.verts, triangles=self.tris, mode='ngon', thickness=2),
+			color=color,
+			texture="white_cube",
+			origin=0,
+			position=position,
+		)
 		self.outline = None
 
-	def update(self):
+	def input(self, key):
 		if self.hovered:
-			print("Hovered")
+			if key == 'left mouse down':
+				print("Clicked")
+				self.color = ursina.rgb(0,0,255)
+				self.animate_color(self.color, duration=.1, interrupt='finish')
+
+			# if key == 'right mouse down':
+			#     destroy(self)
 
 	# def update(self):
 	# 	if self.hovered:
@@ -92,11 +156,11 @@ class Tile(ursina.Entity):
 
 		
 class MenuButton(ursina.Button):
-    def __init__(self, text='', **kwargs):
-        super().__init__(text, scale=(.25, .075), highlight_color=color.azure, **kwargs)
+	def __init__(self, text='', **kwargs):
+		super().__init__(text, scale=(.25, .075), highlight_color=color.azure, **kwargs)
 
-        for key, value in kwargs.items():
-            setattr(self, key ,value)
+		for key, value in kwargs.items():
+			setattr(self, key ,value)
 
 class App(ursina.Ursina):
 	def __init__(self, *args, **kwargs):
@@ -105,7 +169,7 @@ class App(ursina.Ursina):
 		self.editor_camera = ursina.EditorCamera(enabled=False, ignore_paused=True)
 		self.editor_camera.enabled = True
 
-		self.radius_slider = ursina.Slider(0, 25, default=3, step=1, text='Radius', parent=ursina.camera.ui, eternal = True, position = (-0.75,0.45))
+		self.radius_slider = ursina.Slider(0, 25, default=0, step=1, text='Radius', parent=ursina.camera.ui, eternal = True, position = (-0.75,0.45))
 		self.radius_slider.on_value_changed = self.set_radius_multiplier
 
 		self.scale_slider = ursina.Slider(0.1, 25, default=1, step=0.1, text='Scale', parent=ursina.camera.ui, eternal = True, position = (-0.75,0.375))
@@ -118,11 +182,10 @@ class App(ursina.Ursina):
 		self.terrain_scale_slider.on_value_changed = self.set_terrain_scale
 
 		self.button = ursina.Button(parent=ursina.camera.ui, eternal = True, position = (-0.75,0.150), on_click = self.generate_map, scale=0.1)
-
 		self.map_entities = []
 
 		self.scale = 1
-		self.radius = 3
+		self.radius = 0
 		self.terrain_amp = 1
 		self.terrain_scale=1
 		self.generate_map()
@@ -156,116 +219,8 @@ class App(ursina.Ursina):
 				print(e)
 		self.map_entities = draw_honeycomb((0, 0), self.radius, self.scale, self.terrain_amp, self.terrain_scale)
 
+
+
 app = App()
-
-app.run()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-from ursina import *
-
-
-# button_size = (.25, .075)
-button_spacing = .075 * 1.25
-menu_parent = Entity(parent=camera.ui, y=.15)
-main_menu = Entity(parent=menu_parent)
-load_menu = Entity(parent=menu_parent)
-options_menu = Entity(parent=menu_parent)
-
-state_handler = Animator({
-    'main_menu' : main_menu,
-    'load_menu' : load_menu,
-    'options_menu' : options_menu,
-    }
-)
-
-
-# main menu content
-main_menu.buttons = [
-    MenuButton('resume'),
-    MenuButton('new game'),
-    MenuButton('load game', on_click=Func(setattr, state_handler, 'state', 'load_menu')),
-    MenuButton('options', on_click=Func(setattr, state_handler, 'state', 'options_menu')),
-    MenuButton('quit', on_click=Sequence(Wait(.01), Func(sys.exit))),
-]
-for i, e in enumerate(main_menu.buttons):
-    e.parent = main_menu
-    e.y = -i * button_spacing
-
-
-
-# load menu content
-for i in range(3):
-    MenuButton(parent=load_menu, text=f'Empty Slot {i}', y=-i * button_spacing)
-load_menu.back_button = MenuButton(parent=load_menu, text='back', y=((-i-2) * button_spacing), on_click=Func(setattr, state_handler, 'state', 'main_menu'))
-
-
-
-# options menu content
-review_text = Text(parent=options_menu, x=.275, y=.25, text='Preview text', origin=(-.5,0))
-for t in [e for e in scene.entities if isinstance(e, Text)]:
-    t.original_scale = t.scale
-
-text_scale_slider = Slider(0, 2, default=1, step=.1, dynamic=True, text='Text Size:', parent=options_menu, x=-.25)
-def set_text_scale():
-    for t in [e for e in scene.entities if isinstance(e, Text) and hasattr(e, 'original_scale')]:
-        t.scale = t.original_scale * text_scale_slider.value
-text_scale_slider.on_value_changed = set_text_scale
-
-
-
-volume_slider = Slider(0, 1, default=Audio.volume_multiplier, step=.1, text='Master Volume:', parent=options_menu, x=-.25)
-def set_volume_multiplier():
-    Audio.volume_multiplier = volume_slider.value
-volume_slider.on_value_changed = set_volume_multiplier
-
-options_back = MenuButton(parent=options_menu, text='Back', x=-.25, origin_x=-.5, on_click=Func(setattr, state_handler, 'state', 'main_menu'))
-
-for i, e in enumerate((text_scale_slider, volume_slider, options_back)):
-    e.y = -i * button_spacing
-
-
-# animate the buttons in nicely when changing menu
-for menu in (main_menu, load_menu, options_menu):
-    def animate_in_menu(menu=menu):
-        for i, e in enumerate(menu.children):
-            e.original_x = e.x
-            e.x += .1
-            e.animate_x(e.original_x, delay=i*.05, duration=.1, curve=curve.out_quad)
-
-            e.alpha = 0
-            e.animate('alpha', .7, delay=i*.05, duration=.1, curve=curve.out_quad)
-
-            if hasattr(e, 'text_entity'):
-                e.text_entity.alpha = 0
-                e.text_entity.animate('alpha', 1, delay=i*.05, duration=.1)
-
-    menu.on_enable = animate_in_menu
-
-
-background = Entity(model='quad', texture='shore', parent=camera.ui, scale=(camera.aspect_ratio,1), color=color.white, z=1)
-
+ursina.camera.mode = "orthoganol"
 app.run()
